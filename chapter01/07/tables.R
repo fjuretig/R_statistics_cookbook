1ce4dc80b5fcebf6e0aae37c1de60b03704bc3b9
Francisco Juretig <fjuretig@yahoo.com>
2019-03-26 21:23:16 +0000

    added missing metals.csv

--- chapter01/07/tables.R
+++ chapter01/07/tables.R
@@ -5,6 +5,9 @@ data = read.csv("/Users/admin/Documents/R_book/chapter01/07/person_salary.csv",s
 Green = "#71CA97"
 Green2 = "#DeF7E9"
 
+table__out = data.frame(table(data$Career,data$Age))
+colnames(table__out) = c("Career","Age","Freq")
+formattable(table__out,align =c("c","c","c"), list("Freq"= color_tile(Green, Green2)))
 
 formattable(data,align =c("c","c","c","c"), list(
   "Person" = formatter("span", style = ~ style(color = "grey",font.weight = "bold")), 
@@ -15,6 +18,4 @@ formattable(data,align =c("c","c","c","c"), list(
 
 
 
-table__out = data.frame(table(data$Career,data$Age))
-colnames(table__out) = c("Career","Age","Freq")
-formattable(table__out,align =c("c","c","c"), list("Freq"= color_tile(Green, Green2)))
+
--- chapter06/06__03__glm.R
+++ chapter06/06__03__glm.R
@@ -1,4 +1,5 @@
 library(robust) 
+set.seed(1000)
 x1       = rnorm(1000)   
 x2       = rnorm(1000) 
 link_val = 2 + 2*x1 + 5*x2   
@@ -18,10 +19,10 @@ x2       = rnorm(1000)
 link_val = 2 + 2*x1 + 5*x2   
 pr       = 1/(1+exp(-link_val))      
 y        = rbinom(1000,1,pr)   
-x1[1:100] = 10*rnorm(100) 
+x1[1:50] = 10*rnorm(50) 
 df       = data.frame(y=y,x1=x1,x2=x2) 
 glm( y~x1+x2,data=df,family="binomial") 
-robust::glmRob(y~x1+x2,mthod="cubif",data=df,family="binomial") 
+robust::glmRob(y~x1+x2,method="cubif",data=df,family="binomial") 
 
 
 
@@ -32,7 +33,7 @@ x2       = rnorm(1000)
 link_val = 2 + 2*x1 + 5*x2   
 pr       = 1/(1+exp(-link_val))      
 y        = rbinom(1000,1,pr)   
-x1[1:200] = 10*rnorm(200) 
+x1[1:100] = 10*rnorm(100) 
 df       = data.frame(y=y,x1=x1,x2=x2) 
 glm( y~x1+x2,data=df,family="binomial") 
 robust::glmRob(y~x1+x2,mthod="cubif",data=df,family="binomial")
